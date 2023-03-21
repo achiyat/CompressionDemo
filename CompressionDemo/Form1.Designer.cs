5be437026ddf02b0cf282d3aa040d5a66e22085e
achiyat <achiya308@gmail.com>
2023-03-21 12:56:52 +0200

    CompressionDemo

---
+++ CompressionDemo/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CompressionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.checkZip = new System.Windows.Forms.CheckBox();
+            this.checkRar = new System.Windows.Forms.CheckBox();
+            this.txtSourceDirectory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Location = new System.Drawing.Point(76, 62);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(119, 50);
+            this.btnCompress.TabIndex = 0;
+            this.btnCompress.Text = "Compress";
+            this.btnCompress.UseVisualStyleBackColor = true;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click_1);
+            // 
+            // checkZip
+            // 
+            this.checkZip.AutoSize = true;
+            this.checkZip.Location = new System.Drawing.Point(314, 78);
+            this.checkZip.Name = "checkZip";
+            this.checkZip.Size = new System.Drawing.Size(48, 20);
+            this.checkZip.TabIndex = 1;
+            this.checkZip.Text = "Zip";
+            this.checkZip.UseVisualStyleBackColor = true;
+            this.checkZip.CheckedChanged += new System.EventHandler(this.checkZip_CheckedChanged);
+            // 
+            // checkRar
+            // 
+            this.checkRar.AutoSize = true;
+            this.checkRar.Location = new System.Drawing.Point(314, 118);
+            this.checkRar.Name = "checkRar";
+            this.checkRar.Size = new System.Drawing.Size(51, 20);
+            this.checkRar.TabIndex = 2;
+            this.checkRar.Text = "Rar";
+            this.checkRar.UseVisualStyleBackColor = true;
+            this.checkRar.CheckedChanged += new System.EventHandler(this.checkRar_CheckedChanged);
+            // 
+            // txtSourceDirectory
+            // 
+            this.txtSourceDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtSourceDirectory.Location = new System.Drawing.Point(45, 195);
+            this.txtSourceDirectory.Name = "txtSourceDirectory";
+            this.txtSourceDirectory.Size = new System.Drawing.Size(717, 30);
+            this.txtSourceDirectory.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSourceDirectory);
+            this.Controls.Add(this.checkRar);
+            this.Controls.Add(this.checkZip);
+            this.Controls.Add(this.btnCompress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompress;
+        private System.Windows.Forms.CheckBox checkZip;
+        private System.Windows.Forms.CheckBox checkRar;
+        private System.Windows.Forms.TextBox txtSourceDirectory;
+    }
+}
+
---
+++ CompressionDemo/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+
+namespace CompressionDemo
+{
+    public partial class Form1 : Form
+    {
+        public const string ZipExtension = ".zip";
+        public const string RarExtension = ".rar";
+
+        private ICompressionStrategy _compressionStrategy = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // By default, use the built-in ZIP compression strategy
+            _compressionStrategy = new ZipCompressionStrategy();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompress_Click_1(object sender, EventArgs e)
+        {
+            string sourceDirectory = txtSourceDirectory.Text.Trim();
+            string destinationPath = Path.Combine(
+                Path.GetDirectoryName(sourceDirectory),
+                Path.GetFileNameWithoutExtension(sourceDirectory) + _compressionStrategy.FileExtension);
+
+            try
+            {
+                _compressionStrategy.Compress(sourceDirectory, destinationPath);
+                MessageBox.Show("Compression complete.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        
+
+        private void checkZip_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkZip.Checked)
+            {
+                _compressionStrategy = new ZipCompressionStrategy();
+            }
+            else
+            {
+                _compressionStrategy = null;
+            }
+        }
+
+        private void checkRar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkRar.Checked)
+            {
+                _compressionStrategy = new RarCompressionStrategy();
+            }
+            else
+            {
+                _compressionStrategy = null;
+            }
+        }
+
+    }
+
+    public interface ICompressionStrategy
+    {
+        string FileExtension { get; }
+        void Compress(string sourceDirectory, string destinationPath);
+    }
+
+    public class ZipCompressionStrategy : ICompressionStrategy
+    {
+        public string FileExtension => Form1.ZipExtension;
+
+        public void Compress(string sourceDirectory, string destinationPath)
+        {
+            ZipFile.CreateFromDirectory(sourceDirectory, destinationPath);
+        }
+    }
+
+    public class RarCompressionStrategy : ICompressionStrategy
+    {
+        public string FileExtension => Form1.RarExtension;
+
+        public void Compress(string sourceDirectory, string destinationPath)
+        {
+            // TODO: Implement RAR compression
+            throw new NotImplementedException("RAR compression not implemented.");
+        }
+    }
+}
